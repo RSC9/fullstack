ff0ee3e029c746f63b9114908845b64c54d052db
Raul Santa Cruz <raulsantacruz@GDL-i4675.local>
2022-03-08 19:46:04 -0600

    1.3: courseInformation step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,16 +3,19 @@ const Header = (props) => (
 );
 
 const Part = (props) => (
-  <p>{props.part} {props.exercises}</p>
+  <p>{props.part.name} {props.part.exercises}</p>
 );
 
-const Content = (props) => (
-  <>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
-  </>
-);
+const Content = (props) => {
+  console.log('+props+', props);
+  return(
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  )
+};
 
 const Total = (props) => (
   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -37,7 +40,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   );
 }
